95b61a433f54b840b00d046d72f65ae217b5ce6d
MrLentes <mr.lentes@hotmail.com>
2023-09-25 21:22:58 -0300

    Mejoras sugeridas
    Hice las mejoras al codigo que me sugirieron

--- Pre-Entrega1 Valverde Jose/MAIN.js
+++ Pre-Entrega1 Valverde Jose/MAIN.js
@@ -2,6 +2,9 @@
 function ElegirTarea(){
     alert("Por favor elija la consulta que desea hacer")
 let tarea = parseInt(prompt("1.- Consultar los eventos programados. \n2.- Consultar el precio de entradas."))
+while (isNaN(tarea) || tarea === '' || tarea > 2 || tarea < 1){
+    tarea = parseInt(prompt("Ingrese una opcion correcta"))
+}
 switch(tarea){
     case 1:
         ConsultarFecha()
@@ -19,8 +22,8 @@ function ConsultarFecha(){
     alert("Hay eventos en Octubre y Noviembre")
     let elegirFecha
     do {
-        elegirFecha = prompt("Elija que mes quiere consultar \n Octubre o Noviembre")
-    } while (elegirFecha != "Octubre" && elegirFecha != "Noviembre")
+        elegirFecha = prompt("Elija que mes quiere consultar \n Octubre o Noviembre").toLowerCase()
+    } while (elegirFecha != "octubre" && elegirFecha != "noviembre")
     let fecha = elegirFecha == "Octubre" ? "13/10/2023 y 27/10/2023" : "08/11/2023";
     alert("Hay eventos en " + fecha)
 }
@@ -29,12 +32,12 @@ function CalcularPrecio(){
     alert("Elija el tipo de entrada que desee consultar")
     let elegirEntrada
     do {
-        elegirEntrada = prompt("Asientos Generales (General). \n Asientos Ringside (Ringside). \n De pie (Pie).")
-    } while (elegirEntrada != "General" && elegirEntrada != "Ringside" && elegirEntrada != "Pie")
+        elegirEntrada = prompt("Asientos Generales (General). \n Asientos Ringside (Ringside). \n De pie (Pie).").toLowerCase()
+    } while (elegirEntrada != "general" && elegirEntrada != "ringside" && elegirEntrada != "pie")
     let entrada = 2500
-    if (elegirEntrada == "General") entrada *= 1
-    else if (elegirEntrada == "Ringside") entrada *= 1.5
-    else if (elegirEntrada == "Pie") entrada *= 0.70
+    if (elegirEntrada == "general") entrada *= 1
+    else if (elegirEntrada == "ringside") entrada *= 1.5
+    else if (elegirEntrada == "pie") entrada *= 0.70
     alert("La entrada cuesta " + entrada)
 }
 
