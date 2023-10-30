1ebc4cbc720ab1438aef942a407ba2a70f33766b
MrLentes <mr.lentes@hotmail.com>
2023-10-30 17:30:06 -0300

    Añadido el boton luchadores
    Hice que el boton de luchadores nos muestre cartas con la informacion de los luchadores

--- Pre-Entrega1 Valverde Jose/MAIN.js
+++ Pre-Entrega1 Valverde Jose/MAIN.js
@@ -1,22 +1,25 @@
 
-function Luchador(nombre, nacionalidad, lesionado){
+function Luchador(nombre, nacionalidad, record, lesionado){
     this.nombre = nombre,
     this.nacionalidad = nacionalidad,
+    this.record = record,
     this.lesionado = lesionado
 }
 
-const luchador = new Luchador("Mack (El Rayo) Reyna", "Uruguayo", false)
-const luchador1 = new Luchador("Charlie Bullet", "Argentino", false)
-const luchador2 = new Luchador("Comandante Fort", "Argentino", true)
-const luchador3 = new Luchador("RAIJIN", "Argentino", false)
-const luchador4 = new Luchador("Mascara Negra", "Mexicano", false)
-const luchador5 = new Luchador("Leon Maldonado", "Argentino", false)
-const luchador6 = new Luchador("Furia Sagrada jr.", "Uruguayo", true)
-const luchador7 = new Luchador("Ivan (El Vikingo)", "Argentino", false)
-const luchador8 = new Luchador("Angel Suarez", "Argentino", false)
-const luchador9 = new Luchador("Payaso Gomez", "Paraguayo", false)
+const luchador = new Luchador("Mack (El Rayo) Reyna", "Uruguayo", '6 G/ 2 E/ 11 D', false)
+const luchador1 = new Luchador("Charlie Bullet", "Argentino", '9 G/ 0 E/ 3 D', false)
+const luchador2 = new Luchador("Comandante Fort", "Argentino", '3 G/ 0 E/ 14 D', true)
+const luchador3 = new Luchador("RAIJIN", "Argentino", '12 G/ 0 E/ 0 D', false)
+const luchador4 = new Luchador("Mascara Negra", "Mexicano", '11 G/ 0 E/ 6 D', false)
+const luchador5 = new Luchador("Leon Maldonado", "Argentino", '7 G/ 1 E/ 11 D', false)
+const luchador6 = new Luchador("Furia Sagrada jr.", "Uruguayo", '10 G/ 0 E/ 3 D', true)
+const luchador7 = new Luchador("Ivan (El Vikingo)", "Argentino", '10 G/ 0 E/ 10 D', false)
+const luchador8 = new Luchador("Angel Suarez", "Argentino", '11 G/ 0 E/ 8D', false)
+const luchador9 = new Luchador("Payaso Gomez", "Paraguayo", '6 G/ 0 E/ 16 D', false)
+const luchador10 = new Luchador("(El Cordoba) Rodrigo", "Argentino", '1 G/ 0 E/ 0 D', false)
+const luchador11 = new Luchador("El Veterano", "Argentino", '13 G/ 1 E/ 15 D', true)
 
-const luchadores = [luchador, luchador1, luchador2, luchador3, luchador4, luchador5, luchador6, luchador7, luchador8, luchador9]
+const luchadores = [luchador, luchador1, luchador2, luchador3, luchador4, luchador5, luchador6, luchador7, luchador8, luchador9, luchador10, luchador11]
 
 localStorage.setItem('luchadoresData', JSON.stringify(luchadores))
 
@@ -49,9 +52,10 @@ Carta.style.color = "black"
 Carta.style.fontSize = "20px"
 
 document.getElementById("btnEventos").addEventListener("click", Eventos)
+document.getElementById("btnLuchadores").addEventListener("click", Luchadores)
 
 function Eventos(){
-    Carta.innerHTML = "Eventos Programados y Boletos"
+    Carta.innerHTML = "Eventos Programados y Boletos" + "<p>"
     for(let i = 0; i < eventos.length; i++){
         let datos = document.createElement("div")
         datos.innerHTML = eventos[i].fecha + " " + eventos[i].lugar + " / " + eventos[i].local
@@ -69,9 +73,16 @@ function Eventos(){
 function CompraBoletos(base){
     Carta.innerHTML = "Evento " + base.fecha + " " + base.lugar + " / " + base.local
     let localidades = document.createElement("div")
+    localidades.style.padding = "15px"
+    localidades.style.backgroundColor = "black"
+    localidades.style.border = "2px solid #369"
+    localidades.style.borderRadius = "10px"
+    localidades.style.color = "white"
+    localidades.style.fontSize = "22px"
+    localidades.style.fontWeight = "bold"
     localidades.innerHTML = `<p>Entradas Generales - Precio ` + base.precioBase + `<button id="btnComprarBoletosG"> Comprar en TicketMaster </button>` + 
     `<p>Entradas Ringside - Precio ` + base.precioBase * 1.5 + `<button id="btnComprarBoletosR"> Comprar en TicketMaster </button>` + 
-    `<p>Entradas Generales - Precio ` + base.precioBase * 0.7 + `<button id="btnComprarBoletosP"> Comprar en TicketMaster </button>`
+    `<p>Entradas De Pie - Precio ` + base.precioBase * 0.7 + `<button id="btnComprarBoletosP"> Comprar en TicketMaster </button>`
     //Los botones te llevarian a TicketMaster
     Carta.appendChild(localidades)
 }
@@ -88,35 +99,55 @@ function EventosCss(div, button){
     button.style.borderRadius = "10px"
     button.style.padding = "10px 20px"
     button.style.cursor = "pointer"
+    button.style.fontSize = "18px"
     button.addEventListener("mouseover", function () { button.style.backgroundColor = "#0056b3" })
     button.addEventListener("mouseout", function () { button.style.backgroundColor = "#48b8b8" })
 }
 
-//Actualizar
+function Luchadores(){
+    Carta.innerHTML = "Informacion de nuestros Luchadores" + "<p>" + "<p>"
+    luchadores.forEach((luchador) => {
+    const luchadorCarta = document.createElement("div")
 
-function InfoLuchadores(){
-    alert("Que deseas consultar?")
-    let info = parseInt(prompt("1.- Consultar los luchadores de la empresa. \n2.- Consultar los luchadores lesionados."))
-    while (isNaN(info) || info === '' || info > 2 || info < 1){
-    info = parseInt(prompt("Ingrese una opcion correcta. \n1.- Consultar los luchadores de la empresa. \n2.- Consultar los luchadores lesionados."))
-    }
-    if (info == 1) MostrarInfo()
-    else if (info == 2) InfoLesionados()
-}
-function MostrarInfo(){
-    for (let i = 0; i < luchadores.length; i++){
-        alert("Nombre: " + luchadores[i].nombre + "\nNacionalidad: " + luchadores[i].nacionalidad)}
-    alert("Gracias por su consulta")    
-}
-function InfoLesionados(){
-    const lesionados = []
-    luchadores.forEach( (lucha) => {if(lucha.lesionado) lesionados.push(lucha.nombre)} )
-    alert("Los lesionados son: ")
-    for (let i = 0; i < lesionados.length; i++){
-        alert(lesionados[i])
-    }
+    LuchadoresCss(luchadorCarta)
+
+    const nombre = document.createElement("div")
+    nombre.style.fontWeight = "bold"
+    nombre.textContent = "Nombre: " + luchador.nombre
+
+    const nacionalidad = document.createElement("div")
+    nacionalidad.style.color = "#777"
+    nacionalidad.textContent = "Nacionalidad: " + luchador.nacionalidad
+
+    const record = document.createElement("div")
+    record.style.color = "blue"
+    record.textContent = luchador.record
+    console.log(record)
+
+    const lesionado = document.createElement("div")
+    lesionado.style.fontWeight = "bold"
+    lesionado.style.color = "red"
+    lesionado.textContent = luchador.lesionado ? "Lesionado" : " "
+
+    luchadorCarta.appendChild(nombre)
+    luchadorCarta.appendChild(nacionalidad)
+    luchadorCarta.appendChild(record)
+    luchadorCarta.appendChild(lesionado)
+
+    Carta.appendChild(luchadorCarta)
+    })
 }
 
+function LuchadoresCss(cartaL){
+    cartaL.style.backgroundColor = "white"
+    cartaL.style.border = "1px solid #777"
+    cartaL.style.borderRadius = "15px"
+    cartaL.style.padding = "20px"
+    cartaL.style.margin = "20px"
+    cartaL.style.display = "inline-block"
+    cartaL.style.textAlign = "left"
+    cartaL.style.width = "300px"
+}
 
 //document.getElementById("btnIniciarConsulta").addEventListener("click", ElegirConsulta)
 /*
@@ -237,5 +268,28 @@ function CalcularPrecio(){
     alert("La entrada cuesta " + entrada + "$")
 }
 */
+
+/*function InfoLuchadores(){
+    alert("Que deseas consultar?")
+    let info = parseInt(prompt("1.- Consultar los luchadores de la empresa. \n2.- Consultar los luchadores lesionados."))
+    while (isNaN(info) || info === '' || info > 2 || info < 1){
+    info = parseInt(prompt("Ingrese una opcion correcta. \n1.- Consultar los luchadores de la empresa. \n2.- Consultar los luchadores lesionados."))
+    }
+    if (info == 1) MostrarInfo()
+    else if (info == 2) InfoLesionados()
+}
+function MostrarInfo(){
+    for (let i = 0; i < luchadores.length; i++){
+        alert("Nombre: " + luchadores[i].nombre + "\nNacionalidad: " + luchadores[i].nacionalidad)}
+    alert("Gracias por su consulta")    
+}
+function InfoLesionados(){
+    const lesionados = []
+    luchadores.forEach( (lucha) => {if(lucha.lesionado) lesionados.push(lucha.nombre)} )
+    alert("Los lesionados son: ")
+    for (let i = 0; i < lesionados.length; i++){
+        alert(lesionados[i])
+    }
+}*/
 
 //ElegirTarea()
